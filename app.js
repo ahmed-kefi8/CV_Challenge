d16adedebe75ad4347a9f67d0efe79890976c35f
ahmedkefi <ahmedkefi.ing@gmail.com>
2016-04-01 23:18:08 +0100

    added some model attributes in the index.html using the mustache template

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ angular.module('myApp', []).controller('createCvCtrl', ['$scope', function($scop
 
   $scope.user = {
       id: 1,
-    firstname: 'ahmed',
-    lastname: 'kefi',
-    apropos: 'Ingénieur informatique'
+    firstname: 'AHMED',
+    lastname: 'KEFI',
+    apropos: 'INGENIEUR INFORMATIQUE',
+	apropos1: 'DEVELOPPEUR FULL-STACK',
+	compt1: 'JavaScript',
+	compt2: 'AngularJS BackboneJS Ember',
+	compt3: 'Coffee Script , ES 2015',
+	compt4: 'Node.JS',
+	compt5: 'HTML CSS SASS , Git et SCRUM',
+	presentation: 'je suis un ingénieur jeune et motivé',
 
   };
 
