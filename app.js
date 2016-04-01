c7b0990404c98882d28e03a867dbb0968cdb79e7
ahmedkefi <ahmedkefi.ing@gmail.com>
2016-04-01 23:41:20 +0100

    all the attributes are declared in the model and displayed in the web page using the mustache template

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ angular.module('myApp', []).controller('createCvCtrl', ['$scope', function($scop
 	compt4: 'Node.JS',
 	compt5: 'HTML CSS SASS , Git et SCRUM',
 	presentation: 'je suis un ingénieur jeune et motivé',
+	poste: 'Developpeur Full-Stack',
+	employeur: 'Craftacademy',
+	lieu:'Hammamet',
+	duree:'Mars Avril Mai 2016',
+	descr:'Superbe Formation avec Craftacademy',
+	diplome: 'Diplome National Ingenieur',
+	ecole: 'ESPRIT',
+	lieuDip: 'Tunis',
+	annee: '2015',
+	descrDiplome: "Diplôme ingénieur en informatique mention Très Bien à l Ecole Supérieure Privée d Ingénierie et de Technologies (ESPRIT) école accrédité par la Commission des Titres d Ingénieur (CTI) et labellisée EUR-ACE (Accréditation des formations d ingénieurs en Europe)",
+	loisir: 'Science, Sport, Cinema', 
+	association:'Protection des animeaux',
+	email:'ahmedkefi.ing@gmail.com',
+	phone:'(+216)50848529'
+
+	
 
   };
 
