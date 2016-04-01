e9fbeae1ccdadc3e6aba0153ed9448736b7cbfb2
ahmedkefi <ahmedkefi.ing@gmail.com>
2016-04-01 10:34:58 +0100

    some other new changes

--- CV/app/view2/view2.js
+++ CV/app/view2/view2.js
@@ -9,6 +9,14 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', function($scope) {
+
+  $scope.user = {
+    firstname: '',
+    lastname: '',
+    apropos: '',
+
+  };
+
 
 }]);
